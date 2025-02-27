8646ea865647d710e6da02074088f49c72cd0345
Olivier Hériveaux <olivier.heriveaux@ledger.fr>
2025-02-27 18:09:25 +0100

    Added basic integration test

---
+++ tests/fibonacci.rs
@@ -0,0 +1,27 @@
+use armagnac::arm::{Arm7Processor, ArmVersion};
+
+#[test]
+fn fibonacci() {
+    let mut proc = Arm7Processor::new(ArmVersion::V7, 0);
+    const ADDR_RAM: u32 = 0x10000000;
+    const STACK_SIZE: u32 = 1024;
+    proc.map_ram(ADDR_RAM, 1024).unwrap();
+    proc.map(0, include_bytes!("fibonacci.bin")).unwrap();
+    proc.set_sp(ADDR_RAM + STACK_SIZE);
+    proc.registers.r0 = 12; // Fibonacci input value
+    proc.registers.lr = 0xfffffffe;
+
+    // Run maximum 1000 instructions
+    for _ in 0..20000 {
+        proc.stepi().unwrap();
+        // Run util code branches back to initial LR
+        if proc.pc() == 0xfffffffe {
+            break;
+        }
+    }
+
+    // Test fibonacci function result
+    assert_eq!(proc.registers.r0, 144);
+    // Verify stack has been poped correctly
+    assert_eq!(proc.sp(), ADDR_RAM + STACK_SIZE);
+}
