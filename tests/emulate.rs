3b204d77191f103f3ccb3385fc553649e449c64c
Olivier Hériveaux <olivier.heriveaux@ledger.fr>
2025-03-09 23:32:16 +0100

    Merged integration tests in single file

--- tests/emulate.rs
+++ tests/emulate.rs
@@ -1,5 +1,31 @@
 use armagnac::arm::{ArmProcessor, ArmVersion};
 
+#[test]
+fn test_fibonacci() {
+    let mut proc = ArmProcessor::new(ArmVersion::V7M, 0);
+    const ADDR_RAM: u32 = 0x10000000;
+    const STACK_SIZE: u32 = 1024;
+    proc.map_ram(ADDR_RAM, 1024).unwrap();
+    proc.map(0, include_bytes!("fibonacci.bin")).unwrap();
+    proc.set_sp(ADDR_RAM + STACK_SIZE);
+    proc.registers.r0 = 12; // Fibonacci input value
+    proc.registers.lr = 0xfffffffe;
+
+    // Run maximum 1000 instructions
+    for _ in 0..20000 {
+        proc.stepi().unwrap();
+        // Run util code branches back to initial LR
+        if proc.pc() == 0xfffffffe {
+            break;
+        }
+    }
+
+    // Test fibonacci function result
+    assert_eq!(proc.registers.r0, 144);
+    // Verify stack has been poped correctly
+    assert_eq!(proc.sp(), ADDR_RAM + STACK_SIZE);
+}
+
 /// This test runs a call to memcpy to copy a string to a buffer.
 #[test]
 fn test_memcpy() {
