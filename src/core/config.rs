9f6f8ecb9e248caae06308ccdc3562f35920e358
Olivier Hériveaux <olivier.heriveaux@ledger.fr>
2025-06-27 11:47:14 +0200

    Moved Config struct to separate file

---
+++ src/core/config.rs
@@ -0,0 +1,56 @@
+use crate::core::ArmVersion;
+
+/// Configuration builder used to build instances of [`ArmProcessor`].
+pub struct Config {
+    /// Arm architecture version. Must be defined.
+    pub(crate) version: ArmVersion,
+    /// Number of platform specific exceptions.
+    pub(crate) external_exceptions: usize,
+    /// Reservation granule for the local monitor dealing with exclusive accesses.
+    pub(crate) exclusives_reservation_granule: u32,
+}
+
+impl Config {
+    pub fn v6m() -> Self {
+        Self {
+            version: ArmVersion::V6M,
+            external_exceptions: 0,
+            exclusives_reservation_granule: 4,
+        }
+    }
+
+    pub fn v7m() -> Self {
+        Self {
+            version: ArmVersion::V7M,
+            ..Self::v6m()
+        }
+    }
+
+    pub fn v7em() -> Self {
+        Self {
+            version: ArmVersion::V7EM,
+            ..Self::v6m()
+        }
+    }
+
+    pub fn v8m() -> Self {
+        Self {
+            version: ArmVersion::V8M,
+            ..Self::v6m()
+        }
+    }
+
+    /// Sets the number of platform specific exceptions.
+    pub fn external_exceptions(mut self, count: usize) -> Self {
+        self.external_exceptions = count;
+        self
+    }
+
+    /// Sets the Exclusive Reservation Granule.
+    ///
+    /// Value must be a power of two in [4, 512].
+    pub fn exclusives_reservation_granule(mut self, granule: u32) -> Self {
+        self.exclusives_reservation_granule = granule;
+        self
+    }
+}
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -2,15 +2,17 @@
 
 mod arm;
 mod condition;
+mod config;
 mod coprocessor;
 mod exclusive_monitor;
 mod irq;
 mod it_state;
 
 pub use arm::{
-    ArmProcessor, ArmVersion, Config, Effect, Emulator, Event, MapConflict, RunError, RunOptions,
+    ArmProcessor, ArmVersion, Effect, Emulator, Event, MapConflict, RunError, RunOptions,
 };
 pub use condition::Condition;
+pub use config::Config;
 pub use coprocessor::Coprocessor;
 pub use exclusive_monitor::{LocalMonitor, MonitorState};
 pub use irq::Irq;
